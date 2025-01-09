e4b566705e189b516207945d91f1fc036be3f7ec
weversonbarbieri <weverson.barbieri@gmail.com>
2025-01-09 09:51:17 -0300

    commit modifications on .py file

--- src/engineering_database.py
+++ src/engineering_database.py
@@ -6,7 +6,7 @@ from firebase_admin import db
 
 # Initialize Firebase
 if not firebase_admin._apps:
-    cred = credentials.Certificate(r'C:\Language_Projects\Language_Projects\Python\Flagship_1\engineering_database\src\engineering_database\engineering-database-fs1-firebase-adminsdk-38sc1-c22043566d.json')
+    cred = credentials.Certificate(r'https://github.com/weversonbarbieri/engineering_database/blob/master/src/engineering_database.py/engineering-database-fs1-firebase-adminsdk-38sc1-c22043566d.json')
     firebase_admin.initialize_app(cred)
 
 
