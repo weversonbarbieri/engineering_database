dea551d7d6f1b9207ccb1d9cf407eedb8ccb5a1f
weversonbarbieri <weverson.barbieri@gmail.com>
2025-01-14 10:21:34 -0300

    commit modifications done on .py file

--- src/engineering_database.py
+++ src/engineering_database.py
@@ -4,8 +4,8 @@ from firebase_admin import credentials, firestore
 from firebase_admin import db
 import json
 
-
-key_dict = json.loads(st.secrets["textkey"])
+with open("textkey") as k:
+    key_dict = json.loads(st.secrets["textkey"])
 # Initialize Firebase
 if not firebase_admin._apps:
     cred = credentials.Certificate(key_dict)
